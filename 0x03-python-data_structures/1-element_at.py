ea797d7d7fd6d2f2fc3ed16ba68cc784f3d808a1
e-nneka <happyme138@gmail.com>
2022-07-05 03:50:51 -0700

    All task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,10 +3,9 @@
 
 def element_at(my_list, idx):
 
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+
     return (my_list[idx] if 0 <= idx < len(my_list) else None)
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
- print("Element at index {:d} is {}" .format(idx, element_at(my_list, idx)))
+print("Element at index {:d} is {}" .format(idx, element_at(my_list, idx)))
 
